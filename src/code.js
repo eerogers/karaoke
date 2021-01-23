7cd0d03407de87079a58a6738197f6a0e422ade1
Elizabeth Rogers <elle.eels@gmail.com>
2021-01-23 15:48:15 -0500

    form variable logic

--- src/code.js
+++ src/code.js
@@ -1,9 +1,27 @@
-function signin() {
-    window.location.href = "signin.html"
-};
-function signup() {
-    window.location.href = "signin.html"
-};
-function home(){
-    window.location.href = "frontpage.html"
-};
+    function signin() {
+        window.location.href = "signin.html"
+    };
+    function signup() {
+        window.location.href = "signup.html"
+    };
+    function home(){
+        window.location.href = "frontpage.html"
+    };
+var signinId
+var signinPassword
+var signupId
+var signupPassword
+$(document).on("click", "#submitSignin", function(){
+    signinId = $('#signinId').val()
+    signinPassword = $('#signinPassword').val()
+    console.log(signinId, signinPassword)
+    $('#signinId').val('')
+     $('#signinPassword').val('')
+});
+$(document).on("click", "#submitSignup", function(){
+     signupId = $('#signupId').val()
+     signupPassword = $('#signupPassword').val()
+     console.log(signupId, signupPassword)
+     $('#signupId').val('')
+     $('#signupPassword').val('')
+ });
