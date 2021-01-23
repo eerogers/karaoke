9458ea5a3de4485513a36e47ef68cc269e130146
Elizabeth Rogers <elle.eels@gmail.com>
2021-01-23 12:59:42 -0500

    pages and basic connectivity

---
+++ src/code.js
@@ -0,0 +1,9 @@
+function signin() {
+    window.location.href = "signin.html"
+};
+function signup() {
+    window.location.href = "signin.html"
+};
+function home(){
+    window.location.href = "frontpage.html"
+};
