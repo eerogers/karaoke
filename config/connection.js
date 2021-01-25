d2aa282c51e9aed0f3b356751a739c834145bb18
Elizabeth Rogers <elle.eels@gmail.com>
2021-01-25 12:14:01 -0500

    node-modules SQL not fully functional

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+var Sequelize = require("sequelize")
+
+var sequelize = new Sequelize('karaoke_db', 'root', 'root', {
+    host: 'localhost',
+    dialect: 'mysql',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000
+    },
+    operatorsAliases: false
+  });
+
+module.exports = sequelize
