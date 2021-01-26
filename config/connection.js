6e88ecc5767ddfa011876a2f37e3629aa6a216ab
Elizabeth Rogers <elle.eels@gmail.com>
2021-01-26 14:36:39 -0500

    database connection fix

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
-var Sequelize = require("sequelize")
+//var Sequelize = require("sequelize")
 
-var sequelize = new Sequelize('karaoke_db', 'root', 'root', {
-    host: 'localhost',
-    dialect: 'mysql',
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000
-    },
-    operatorsAliases: false
-  });
+//var sequelize = new Sequelize('karaoke_db', 'root', 'root', {
+//    host: 'localhost',
+//    dialect: 'mysql',
+//    pool: {
+//      max: 5,
+//      min: 0,
+//      acquire: 30000
+//    },
+//    operatorsAliases: false
+//  });
 
-module.exports = sequelize
+//module.exports = sequelize
